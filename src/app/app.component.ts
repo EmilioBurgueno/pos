9abfd5c6007e6f73af27e066fa21da2bde4971f8
Luis Enrique Hernandez Sandoval <luisenrique@mbp-de-luis-v2.lan>
2020-04-13 18:13:51 -0700

    Inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,37 +13,21 @@ export class AppComponent implements OnInit {
   public selectedIndex = 0;
   public appPages = [
     {
-      title: 'Inbox',
-      url: '/folder/Inbox',
-      icon: 'mail'
+      title: 'Checkout',
+      url: '/menu/checkout',
+      icon: 'receipt'
     },
     {
-      title: 'Outbox',
-      url: '/folder/Outbox',
-      icon: 'paper-plane'
+      title: 'Transactions',
+      url: '/menu/transactions',
+      icon: 'swap-vertical'
     },
     {
-      title: 'Favorites',
-      url: '/folder/Favorites',
-      icon: 'heart'
-    },
-    {
-      title: 'Archived',
-      url: '/folder/Archived',
-      icon: 'archive'
-    },
-    {
-      title: 'Trash',
-      url: '/folder/Trash',
-      icon: 'trash'
-    },
-    {
-      title: 'Spam',
-      url: '/folder/Spam',
-      icon: 'warning'
+      title: 'Items',
+      url: '/menu/items',
+      icon: 'list'
     }
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
 
   constructor(
     private platform: Platform,
